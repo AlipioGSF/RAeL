c81b7958aa44561dfb8079dfa1b8b6e447458c1d
Alipio Gomes <alipiotec@gmail.com>
2022-06-10 00:53:20 -0300

    Criando RA&L

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        colorPallet:{
+          10: "#F08434",
+          12: "#BD692A",
+          17: "#F2B07E",
+          20: "#70523A",
+          22: "#703F19",
+        }, 
+      }
+    },
+  },
+  plugins: [],
+}
